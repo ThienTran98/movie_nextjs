5b31595024c3c0299a2e34e0bb28e8172ed7fd41
TranThien <tranhuuthien98@gmail.com>
2024-03-28 13:14:09 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "../styles/index.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import { store } from "./store";
+
 import { Fragment } from "react";
+import { store } from "@/lib/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
